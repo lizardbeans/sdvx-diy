7182dc98e8424791135a2c8ad2c9f1c01d4e28fa
Cris <chantobal666@gmail.com>
2017-06-23 17:56:20 -0400

    Fixed leovxhq

--- code Leonardo/Arduino Leonardo/leovxhq/leovxhq.ino
+++ code Leonardo/Arduino Leonardo/leovxhq/leovxhq.ino
@@ -8,34 +8,36 @@ iivxReport_t report;
 #define ENC_L_A 0
 #define ENC_L_B 1
 #define ENC_L_B_ADDR 3
+#define ENC_R_A 2
+#define ENC_R_B 3
+#define ENC_R_B_ADDR 0
 #define ENCODER_SENSITIVITY (double) 2.34375
 #define ENCODER_PORT PIND
-// encoder sensitivity = number of positions per rotation (600) / number of positions for HID report (256)
+// encoder sensitivity = number of positions per rotation (600/256) / number of positions for HID report (256)
 /*
  * connect encoders
- * TURNTABLE to pin 0 and 1
+ * VOL-L to pins 0 and 1
+ * VOL-R to pins 2 and 3
  */
 
 int tmp;
-uint8_t buttonCount = 9;
+uint8_t buttonCount = 7;
 uint8_t lightMode = 1;
 // 0 = reactive lighting, 1 = HID lighting
-uint8_t ledPins[] = {2,3,4,5,6,7,8,9,10};
-uint8_t buttonPins[] = {11,12,13,18,19,20,21,22,23};
-//uint8_t sysPin = 11;
+uint8_t ledPins[] = {6,7,8,9,10,11,12};
+uint8_t buttonPins[] = {13,18,19,20,21,22,23};
+uint8_t sysPin = 5;
 uint8_t reactiveLightPin = 21;
 uint8_t hidLightPin = 22;
 uint8_t sysInputPins[] = {13,18,19,20};
-int32_t encL=0;
+int32_t encL=0, encR=0;
 /* current pin layout
- *  pins 18 - 23 = A0 - A5
- *  pins 2 to 10 = LED 1 to 7
+ *  pins 6 to 12 = LED 1 to 7
  *    connect pin to + termnial of LED
  *    connect ground to resistor and then - terminal of LED
- *  pins 11 and 12 = START and EFFECT
  *  pins 13, A0 to A5 = Button input 1 to 7
  *    connect button pin to ground to trigger button press
- *  pins 11 = system pin
+ *  pins 5 = system pin
  *    connect system pin to ground with a small button
  *      press together with other buttons to change lighting scheme
  *    system button + button 1 = reactive lighting
@@ -48,6 +50,14 @@ void doEncL(){
     encL++;
   } else {
     encL--;
+  }
+}
+
+void doEncR(){
+  if((ENCODER_PORT >> ENC_R_B_ADDR)&1){
+    encR++;
+  } else {
+    encR--;
   }
 }
 
@@ -68,11 +78,14 @@ void setup() {
     pinMode(buttonPins[i],INPUT_PULLUP);
     pinMode(ledPins[i],OUTPUT);
   }
-  //pinMode(sysPin,INPUT_PULLUP);
+  pinMode(sysPin,INPUT_PULLUP);
   //setup interrupts
   pinMode(ENC_L_A,INPUT_PULLUP);
   pinMode(ENC_L_B,INPUT_PULLUP);
+  pinMode(ENC_R_A,INPUT_PULLUP);
+  pinMode(ENC_R_B,INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_R_A), doEncR, RISING);
 }
 
 void loop() {
@@ -86,6 +99,7 @@ void loop() {
   }
   // Read Encoders
   report.xAxis = (uint8_t)((int32_t)(encL / ENCODER_SENSITIVITY) % 256);
+  report.yAxis = (uint8_t)((int32_t)(encR / ENCODER_SENSITIVITY) % 256);
   // Light LEDs
   if(lightMode==0){
     lights(report.buttons);
@@ -93,21 +107,21 @@ void loop() {
     lights(iivx_led);
   }
   // Detect Syspin Entries
-  //if(digitalRead(buttonPins[0])!=HIGH){
-  //  report.buttons = 0;
-  //  for(int i=0;i<4;i++){
-  //    if(digitalRead(sysInputPins[i])!=HIGH){
-  //      report.buttons |= (uint16_t)1 << (i+buttonCount);
-  //    } else {
-   //     report.buttons &= ~((uint16_t)1 << (i+buttonCount));
- //     }
-  //  }
- //   if(digitalRead(reactiveLightPin)!=HIGH){
-  //    lightMode = 0;
- //   } else if (digitalRead(hidLightPin)!=HIGH){
- //     lightMode = 1;
- //   }
- // }
+  if(digitalRead(sysPin)!=HIGH){
+    report.buttons = 0;
+    for(int i=0;i<4;i++){
+      if(digitalRead(sysInputPins[i])!=HIGH){
+        report.buttons |= (uint16_t)1 << (i+buttonCount);
+      } else {
+        report.buttons &= ~((uint16_t)1 << (i+buttonCount));
+      }
+    }
+    if(digitalRead(reactiveLightPin)!=HIGH){
+      lightMode = 0;
+    } else if (digitalRead(hidLightPin)!=HIGH){
+      lightMode = 1;
+    }
+  }
   // Send report and delay
   iivx.setState(&report);
   delayMicroseconds(REPORT_DELAY);
